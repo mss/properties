752e248e494ab8034884ef05eeb72afef6b1ca52
Malte S. Stretz <stretz@silpion.de>
2013-09-25 14:48:33 +0800

    Initialize Maven project.

---
+++ src/main/java/de/msquadrat/properties/App.java
@@ -0,0 +1,13 @@
+package de.msquadrat.properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
