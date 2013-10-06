2d13197d7f3143e7df8ba6c84f5c09da729e125c
Malte S. Stretz <stretz@silpion.de>
2013-10-06 14:07:21 +0200

    Strip down main boilerplate.

---
+++ src/main/java/de/msquadrat/properties/Application.java
@@ -0,0 +1,11 @@
+package de.msquadrat.properties;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        new Application(args);
+    }
+    
+    protected Application(String[] args) throws Exception {
+        throw new RuntimeException("Not implemented.");
+    }
+}
